704d198edf17128208b0e41f96ec050043070d35
emiliopardo <e.pardo.perez@gmail.com>
2021-07-20 07:36:32 +0000

    Deploying to gh-pages from  @ f8eec5fe363da356bc239db1e2365a43212166d9 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -2813,8 +2813,8 @@ var mapView = function () {
     $('.map').show();
     $('#map').sdgMap({
       indicatorId: indicatorId,
-      mapOptions: null,
-      mapLayers: null,
+      mapOptions: {"minZoom":5,"tileURL":"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png","tileOptions":{"attribution":"My map attribution"}},
+      mapLayers: [{"min_zoom":5,"max_zoom":16,"subfolder":"regions","label":"My map layer label (can be a translation key)"}],
       precision: precision,
       decimalSeparator: decimalSeparator,
     });
